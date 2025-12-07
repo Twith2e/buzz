1f2371d7bbaef0e36d753ea8cba107caaa5e906c
Twith2e <abdulbasitadebajo1619@gmail.com>
2025-12-07 17:47:55 +0100

    replaced baseurl api

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:3001/api/v1",
-  baseURL: "https://tapo-server.onrender.com",
+  baseURL: "https://tapo-server.onrender.com/api/v1",
   withCredentials: true,
 });
 
